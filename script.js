7e4522c243031db21b652c52941bdc6bc9ada719
parth123046 <147490278+parth123046@users.noreply.github.com>
2024-02-01 21:11:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function checkBirthdays(data) {
             </div>
         `;
     } else if (matchingBirthdays.length > 1) {
-        birthdayListHTML = '<div class="birthday"><p> These are celebrating birthdays today! 🎉🎂 :</p>';
+        birthdayListHTML = '<div class="birthday"><p> They are celebrating their birthdays today! 🎉🎂 :</p>';
         matchingBirthdays.forEach(person => {
             birthdayListHTML += `<p><span class="name">${person.name}</span> from class ${person.class} section ${person.section}</p>`;
         });
