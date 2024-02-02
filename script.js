de1eb8e3e958dda69f4a538ca3aa2cd726e6f320
parth123046 <147490278+parth123046@users.noreply.github.com>
2024-02-02 16:45:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: 'John Doe', birthday: '29-01', class: 'X', section: 'A' },
         { name: 'Jane Smith', birthday: '15-05', class: 'XI', section: 'B' },
         { name: 'Michael Johnson', birthday: '10-12', class: 'XII', section: 'C' },
-        { name: 'Alice Johnson', birthday: '02-02', class: 'IX', section: 'D' } 
-        { name: 'AARVIKA KSHITIJ TARGE', birthday: '02-03' },
-        { name: 'ANAYRA ADITYA VENUGOPAL', birthday: '19-02' },
-        { name: 'HARSHIT MANISH TATIYA', birthday: '21-04' },
-        { name: 'TASHVIKA AKASH SACHDEV', birthday: '01-02' },
-        { name: 'AARYA YASHWANT SONAWANE', birthday: '08-01' },
-        { name: 'ANUP AMOL GITE', birthday: '20-04' },
-        { name: 'GARGI HANSRAJ DEORE', birthday: '27-05' },
-        { name: 'HRIDYA SUMIT CHORDIYA', birthday: '15-12' },
-        { name: 'KAVISH PRAVIN GITE', birthday: '10-01' },
-        { name: 'KAVYANSH DHANRAJ DHATRAK', birthday: '06-03' },// Added birthday for 1st February
+        { name: 'Alice Johnson', birthday: '02-02', class: 'IX', section: 'D' } // Added birthday for 1st February
     ];
 
     checkBirthdays(birthdays);
@@ -33,13 +23,13 @@ function checkBirthdays(data) {
         const person = matchingBirthdays[0];
         birthdayListHTML = `
             <div class="birthday">
-                <p><span class="name">${person.name}</span> from class ${person.class} section ${person.section} is celebrating their birthday today! 🎉🎂</p>
+                <p><span class="name">${person.name}</span>  is celebrating their birthday today! 🎉🎂</p>
             </div>
         `;
     } else if (matchingBirthdays.length > 1) {
-        birthdayListHTML = '<div class="birthday"><p>Celebrating birthdays today:</p>';
+        birthdayListHTML = '<div class="birthday"><p>They are celebrating their birthdays today:</p>';
         matchingBirthdays.forEach(person => {
-            birthdayListHTML += `<p><span class="name">${person.name}</span> from class ${person.class} section ${person.section}</p>`;
+            birthdayListHTML += `<p><span class="name">${person.name}</span> </p>`;
         });
         birthdayListHTML += '</div>';
     } else {
