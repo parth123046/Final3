d158e9863ed35881a6d8d82ae5aece5ba57d44fb
shantanusonawane30 <157087036+shantanusonawane30@users.noreply.github.com>
2024-02-05 14:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const birthdays = [
        
         { name: 'Alice Johnson', birthday: '02-02' },
-        { name: 'AARVIKA KSHITIJ TARGE', birthday:'02-03'},
-        { name: 'ANAYRA ADITYA VENUGOPAL', birthday:'19-02'}, 
-        { 'name': 'HARSHIT MANISH TATIYA', 'birthday': '21-04' },
-        { 'name': 'TASHVIKA AKASH SACHDEV', 'birthday': '01-02' },
-        { 'name': 'AARYA YASHWANT SONAWANE', 'birthday': '08-01' },
-        { 'name': 'ANUP AMOL GITE', 'birthday': '20-04' },
-        { 'name': 'GARGI HANSRAJ DEORE', 'birthday': '27-05' },
-        { 'name': 'HRIDYA SUMIT CHORDIYA', 'birthday': '15-12' },
-        { 'name': 'KAVISH PRAVIN GITE', 'birthday': '10-01' } 
-     ];
+        { name: 'Alice Johnson', birthday: '02-02' },
+        { name: 'AARVIKA KSHITI Targe', birthday: '02-03' },
+        { name: 'ANAYRA ADITYA VENUGOPAL', birthday: '19-02' },
+        { name: 'HARSHIT MANISH TATIYA', birthday: '21-04' },
+        { name: 'TASHVIKA AKASH SACHDEV', birthday: '01-02' },
+        { name: 'AARYA YASHWANT SONAWANE', birthday: '08-01' },
+        { name: 'ANUP AMOL GITE', birthday: '20-04' },
+        { name: 'GARGI HANSRAJ DEORE', birthday: '27-05' },
+        { name: 'HRIDYA SUMIT CHORDIYA', birthday: '15-12' },
+        { name: 'KAVISH PRAVIN GITE', birthday: '10-01' }
+    
+];
+
+     
     
 
         
